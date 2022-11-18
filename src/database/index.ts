340ed4eb40e780a23e4577068b2a88e0664890bc
INTRA\ts-long.tran <Long Tran>
2022-11-18 15:48:00 +0700

    fix pool

--- src/database/index.ts
+++ src/database/index.ts
@@ -13,25 +13,20 @@ const {
   ENV,
 } = process.env;
 
-let client = new Pool();
-if (ENV === 'dev') {
-  console.log('dev-dev');
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: String(POSTGRES_PASSWORD),
-    port: Number(POSTGRES_PORT),
-  });
-}
+let client = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: Number(POSTGRES_PORT)
+});
 if (ENV === 'test') {
-  console.log('test-test');
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
-    password: String(POSTGRES_PASSWORD),
-    port: Number(POSTGRES_PORT),
+    password: POSTGRES_PASSWORD,
+    port: Number(POSTGRES_PORT)
   });
 }
 export default client;
